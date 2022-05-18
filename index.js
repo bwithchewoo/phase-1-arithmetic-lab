d04ab3634206944a19648daa27e73b961a200ac0
Anna Cho <loveablessing@gmail.com>
2022-05-18 17:47:15 -0400

    commit tree 1a506e95c5480984a8075ff581c49c5fda23c125

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 19;
+let num4 = 5;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()) + 1;
+let mod = num3 % num4;
+let max = Math.max(1,2,3,5,20);
+const age = 6;
+const isAdult = age >= 18;
+console.log(isAdult);
